29321a95ce7cb47febb105089b6130a0e04c02af
jeff <jeffmitchell33@gmail.com>
2017-12-12 23:53:54 -0500

    First attempt at CORS API call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,134 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+//Make a call to GDAX
+
+var crypto = require('crypto');
+
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest !== "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
   }
+  return xhr;
 }
 
-export default App;
+
+function makeCorsRequest(url) {
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    throw new Error('CORS not supported');
+    return;
+  } 
+
+
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    console.log('Response from CORS request to ' + url + 'is : ' + text);
+  }
+
+  xhr.onerror = function() {
+    alert('Error making the request');
+  }
+
+
+  xhr.send()
+}
+
+export default class TokenList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      object : []
+      // MarketCurrency : [],
+      // BaseCurrency : [],
+      // MarketCurrencyLong : [],
+      // BaseCurrencyLong : [],
+      // MinTradeSize: [],
+      // isActive : [],
+      // LogoUrl : []
+    };
+  }
+
+  componentDidMount() {
+    this.TokenList();
+  }
+  
+//getcurrencies - transaction costs
+//getmarkets - current liquidity
+
+  TokenList() {
+    
+    // Neither method currently working...
+
+    // Method 1
+    $.ajax({
+      type: 'GET',
+      url: 'https://bittrex.com/api/v1.1/public/getmarketsummaries',
+      contentType: 'text/plain',
+      xhrFields: {
+        withCredentials: false
+      },
+      
+      success: function() {
+        console.log('Access Granted!');
+      },
+
+      headers: {
+        'Access-Control-Allow-Origin' : 'http://localhost:3000'
+      },
+
+      error: function() {
+        alert('Error making the request');
+      }
+
+    })
+    
+
+
+    // Method 2
+    // $.getJSON(makeCorsRequest('https://bittrex.com/api/v1.1/public/getmarketsummaries'))
+    //  .then(({  results  }) => this.setState({ object: results }));
+  }
+
+
+render() {
+  const object = this.state.object.map((item, i) => (
+    <div>
+      <h1>{ item }</h1>
+    </div>
+    ));
+
+  return(
+    <div className="TokenList">
+      { object }
+    </div>
+    );
+  }
+
+}
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TokenList from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TokenList />, document.getElementById('root'));
 registerServiceWorker();
+
+if (module.hot) {
+	module.hot.accept();
+}
